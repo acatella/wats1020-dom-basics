57a43d860d9dc1c2b6f54fee56ed6f8fdb613cef
acatella <aficclark@gmail.com>
2016-01-24 15:53:36 -0800

    added and style input buttons, created howManyAte function

--- fortune-cookie-generator.js
+++ fortune-cookie-generator.js
@@ -1,67 +1,57 @@
-//////////////////////////////////////////////////////////////////
-//
+
 // Fortune Cookie Generator
-//
-//////////////////////////////////////////
-
-
-    // This is where your code for the Fortune Cookie generator goes.
-    // You will use the fortunesList variable defined lower in this file
-    // to supply your fortune cookies with text.
-
-    // TODO: Grab the paragraph with the ID
-    // `fortune-cookie-text` to be able to insert text into that element.
-
-    // TODO: Update the Previous Fortunes list with the current `innerHTML`
-    // value of `#fortune-cookie-text`. Follow these steps:
-        // 1. Create a new `li` element with the `document.createElement()` method.
-        // 2. Set the `innerHTML` of that element equal to the `innerHTML` of
-        //    the `#fortune-cookie-text` element.
-        // 3. Select the `#previous-fortunes-container` container and use
-        //    `appendChild()` to append the new `li` element you created above.
-        // 4. You should see the previous fortune cookie saying show up in the list.
-
-    // TODO: Select a new (random) fortune cookie saying from the data stored in the
-    // `fortunesList` variable. (HINT: You will use `Math.floor()` and
-    // `Math.random()` to accomplish this.) Use this data to update the
-    // `innerText` of the `#fortune-cookie-text` element.
+// by Afi C. Clark
 
     //TODO STRETCH:
-    // Prevent duplicate fortunes
     // Style the page nicely
     // Show usage stats
     // Fortunes for multiple people?
 
+//Decides how many people need fortunes
+var numberOfFortunes = 1;
+function howManyAte(input) {
+  numberOfFortunes = input;
+}
+
 var generateFortuneCookie = function() {
-  //Create element to display
+  //Create element to display and usedList
   var newFortuneElement = document.createElement("P");
+
   //Choose which fortune to use
-  //Check to see if all fortunes have been shown
-  if (!(fortuneList[0])) {
-    fortuneList == usedList;
+    //Check to see if all fortunes have been shown
+  if (fortuneList.length == 0) {
+    //if so, build a new fortuneList array out of contents of usedList
+    usedStr = usedList.join("~~");
+    fortuneList = usedStr.split("~~");
+    //clear the usedList array and previousFortunes list
     usedList.splice(0,usedList.length);
+    previousFortunes.innerHTML = "";
   }
-    //Random using math .floor
+    //Get a random fortune using math.floor
   var fortuneNumber = Math.floor(Math.random() * fortuneList.length)
   var fortuneText = fortuneList[fortuneNumber];
-
     //Remove chosen fortune from fortuneList
   fortuneList.splice(fortuneNumber, 1);
-    //Add chosen fortune to a new array, usedList
-  var usedList = [];
+    //Add chosen fortune to usedList
   usedList.push(fortuneText);
+
   //Add fortune text to the element, set ID
   newFortuneElement.setAttribute("id","js-fortuneText");
   newFortuneElement.innerHTML = fortuneText;
+
   //Replace default text or current fortune w/ new fortune
   fortuneElement = document.getElementById("js-fortuneText");
   fortuneElement.parentNode.replaceChild(newFortuneElement,fortuneElement);
+
   //Add new fortune element to previous fortunes list by creating LI w/ same text
   var newUsedFortune = document.createElement("LI");
   newUsedFortune.innerHTML = fortuneText;
+  previousFortunes = document.getElementById("js-previousFortunes");
+  previousFortunes.appendChild(newUsedFortune);
 }
 
-// The following data list is provided for you to use in your code.
+// Data lists
+var usedList = [];
 var fortuneList = [
     "People are naturally attracted to you.",
     "You learn from your mistakes... You will learn a lot today.",
